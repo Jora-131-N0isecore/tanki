7b3d828d09795b528dfdae626e2de24b027b5db4
Жора Жоравич Жирнинг <fedosimovegor880@gmail.com>
2026-06-02 17:47:58 +0300

    вырезал настройки
    всё равно их не будет

--- frame/frames.py
+++ frame/frames.py
@@ -1,19 +1,10 @@
 import tkinter as tk
 
 
-def show_settings():
-    MainMenu.pack_forget()
-    settings_frame.pack(fill="both", expand=True)
-
-
 def show_level_settings():
     MainMenu.pack_forget()
     level_selection_frame.pack(fill="both", expand=True)
 
-
-def show_main():
-    settings_frame.pack_forget()
-    MainMenu.pack(fill="both", expand=True)
 
 def show_main2():
     level_selection_frame.pack_forget()
@@ -27,7 +18,7 @@ def create_window():
     root.title("ТАНКИ2026")
     root.resizable(False, False)
     root.attributes('-fullscreen', True)
-    global MainMenu, settings_frame, level_selection_frame
+    global MainMenu, level_selection_frame
     def close_window():
         root.destroy()
 
@@ -36,16 +27,8 @@ def create_window():
 
     tk.Label(MainMenu, text="ГЛАВНОЕ МЕНЮ", font=("Arial", 20)).pack(pady=20)
     tk.Button(MainMenu, text="Играть", font=("Arial", 10), command=show_level_settings).pack(pady=10)
-    tk.Button(MainMenu, text="Настройки", font=("Arial", 10), command=show_settings).pack(pady=10)
     tk.Button(MainMenu, text="Выход", font=("Arial", 10), command=close_window).pack(pady=10)
     MainMenu.pack(fill="both", expand=True)
-
-
-    #тут меню настроек
-    settings_frame = tk.Frame(root)
-    tk.Label(settings_frame, text="Настройки", font=("Arial", 20)).pack(pady=20)
-    tk.Button(settings_frame, text="Назад", command=show_main).pack(pady=10)
-    tk.Button(settings_frame, text="Пустая кнопка").pack(pady=10)
 
     #меню выбора уровней
     level_selection_frame = tk.Frame(root)
